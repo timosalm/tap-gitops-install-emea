974e70a2a2f962ed5444fbeb6e6626df5c3c029f
Timo Salm <tsalm@vmware.com>
2023-04-13 17:49:32 +0200

    Add initial main cluster tap configuration

--- clusters/main-cluster/tanzu-sync/scripts/deploy.sh
+++ clusters/main-cluster/tanzu-sync/scripts/deploy.sh
@@ -2,9 +2,4 @@
 set -o errexit -o nounset -o pipefail
 #set -o xtrace
 
-kapp deploy -a tanzu-sync \
-  -f <(ytt -f tanzu-sync/app/config \
-           -f cluster-config/config/tap-install/.tanzu-managed/version.yaml \
-           --data-values-file tanzu-sync/app/values/ \
-           --data-values-file sensitive-values.sh.stdout=<(tanzu-sync/scripts/sensitive-values.sh) \
-      ) $@
+ytt -f tanzu-sync/app/config -f cluster-config/config/tap-install/.tanzu-managed/version.yaml --data-values-file tanzu-sync/app/values/  --ignore-unknown-comments | kapp deploy -a tanzu-sync -f- --diff-changes --yes
